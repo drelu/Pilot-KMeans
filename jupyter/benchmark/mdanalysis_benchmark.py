6c8593c3339d3996c903df890a31d12bcf16977e
Andre Luckow <andre.luckow@gmail.com>
2015-11-28 20:39:14 +0000

    working on benchmarks for all pairs

--- jupyter/benchmark/mdanalysis_benchmark.py
+++ jupyter/benchmark/mdanalysis_benchmark.py
@@ -2,16 +2,80 @@ from MDAnalysis.core.distances import distance_array, self_distance_array
 from MDAnalysis.analysis.distances import contact_matrix
 import numpy as np
 import time, os, sys, gc
+from pilot_hadoop import PilotComputeService as PilotSparkComputeService
 
-files=["../md_centered.xtc_95Atoms.np_txt", "../md_prod_12x12_everymicroS_pbcmolcenter.xtc_44784Atoms.np_txt", "../vesicle_1_5M_373_stride1000.xtc_145746Atoms.np_txt"]
+execfile("../util/init_spark.py")
 
-results=[]
-for f in files:
-    #print f
-    coord = np.loadtxt(f, dtype='float32')
+files=["../md_centered.xtc_95Atoms.np_txt", 
+       "../md_prod_12x12_everymicroS_pbcmolcenter.xtc_44784Atoms.np_txt", 
+       "../vesicle_1_5M_373_stride1000.xtc_145746Atoms.np_txt"]
+
+
+################################################################################
+# Process one point at a time
+def get_edges_point(point_index, adjacency_matrix, cutoff=15.0):
+    edge_list = []
+    for index, i in np.ndenumerate(adjacency_matrix):
+        #print ("Index: %d, Value: %d"%(index[i], i))
+        #if point_index<=index[1] and i<cutoff:
+        if i==True and point_index<=index[1]:
+            # Attention we only compute the upper half of the adjacency matrix
+            # thus we need to offset the target edge vertice by point_index
+            edge_list.append((point_index, point_index+index[1]))
+    #del adjacency_matrix
+    return edge_list
+
+
+def compute_distance(point_index):
+    # 1-D Partitioning
+    coord_all = coord_broadcast.value
+    coord_part = coord_all[point_index-1:point_index]
+    #adj = (distance_array(coord_part, coord_all[point_index:], box=None) < cutoff)
+    adj = (cdist(coord_part, coord_all) < cutoff)
+    #adj = cdist(coord_part, coord_all)
+    edge_list = get_edges_point(point_index, adj)
+    del adj
+    #del coord_part
+    #del coord_all
+    #gc.collect()
+    return edge_list
+
+
+def benchmark_spark(coord, NUMBER_EXECUTORS):
+    pilotcompute_description = {
+        "service_url": "yarn-client://yarn.radical-cybertools.org",
+        "number_of_processes": NUMBER_EXECUTORS,
+        "physical_memory_per_process": "3G" 
+    }
+
+    print "SPARK HOME: %s"%os.environ["SPARK_HOME"]
+    print "PYTHONPATH: %s"%os.environ["PYTHONPATH"]
+
+    start = time.time()
+    pilot_spark = PilotSparkComputeService.create_pilot(pilotcompute_description=pilotcompute_description)
+    sc = pilot_spark.get_spark_context()
+    print "SparkStartup, %.2f"%(time.time()-start)
+    coord_broadcast = sc.broadcast(coord)    
+    part_rdd=sc.parallelize(range(len(coord_broadcast.value)), NUMBER_EXECUTORS)
+    part_rdd.cache()
+    start = time.time()
+    edges_list=part_rdd.map(compute_distance).flatMap(lambda a: a).collect()
+    print "ComputeDistanceSpark, %d, %d, %.2f"%(len(coord_all), NUMBER_EXECUTORS, (time.time()-start))
+    pilot_spark.cancel()
+
+def benchmark_mdanalysis()
     start = time.time()
     #distance_array(coord, coord, box=None)
     contact_matrix(coord, returntype="sparse")
     print "ComputeDistanceMDAnalysis, %d, %.2f"%(len(coord), (time.time()-start))
+     
+        
+results=[]
+for f in files:
+    print f
+    coord = np.loadtxt(f, dtype='float32')
+    benchmark_spark(coord, 48)
     del coord
     gc.collect()
+    
+    
--- jupyter/util/init_spark.py
+++ jupyter/util/init_spark.py
@@ -1,7 +1,9 @@
 import os, sys, time
 
+SPARK_HOME="/usr/hdp/2.3.2.0-2950/spark-1.6.0P2" 
+SPARK_HOME="/usr/hdp/2.3.2.0-2950/spark-1.5.2-bin-hadoop2.6" 
+
 if not os.environ.has_key("SPARK_HOME"):
-    SPARK_HOME="/usr/hdp/2.3.2.0-2950/spark-1.5.1-bin-hadoop2.6/"    
     os.environ["SPARK_HOME"]=SPARK_HOME
 else:
     SPARK_HOME=os.environ["SPARK_HOME"]
@@ -15,6 +17,7 @@ os.environ["PYSPARK_DRIVER_PYTHON_OPTS"]="notebook"
 os.environ["PYTHONPATH"]= os.path.join(SPARK_HOME, "python")+":" + os.path.join(SPARK_HOME, "python/lib/py4j-0.8.2.1-src.zip")
     
 sys.path.insert(0, os.path.join(SPARK_HOME, "python"))
+#sys.path.insert(0, os.path.join(SPARK_HOME, 'python/lib/py4j-0.9-src.zip')) 
 sys.path.insert(0, os.path.join(SPARK_HOME, 'python/lib/py4j-0.8.2.1-src.zip')) 
 sys.path.insert(0, os.path.join(SPARK_HOME, 'bin') )
 
