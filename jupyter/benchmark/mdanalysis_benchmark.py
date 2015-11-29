df59cb12e4a004bb91e7560342b151fc989ada76
Andre Luckow <andre.luckow@gmail.com>
2015-11-29 01:07:02 +0000

    refinement of benchmark scripts

--- jupyter/benchmark/mdanalysis_benchmark.py
+++ jupyter/benchmark/mdanalysis_benchmark.py
@@ -1,15 +1,30 @@
 from MDAnalysis.core.distances import distance_array, self_distance_array
 from MDAnalysis.analysis.distances import contact_matrix
+import scipy.sparse
+from scipy.spatial.distance import cdist
 import numpy as np
 import time, os, sys, gc
-from pilot_hadoop import PilotComputeService as PilotSparkComputeService
+import datetime
+import logging
+logger = logging.getLogger("py4j")
+logger.setLevel(logging.ERROR)
 
 execfile("../util/init_spark.py")
+from pilot_hadoop import PilotComputeService as PilotSparkComputeService
+
+RESULT_DIR="results"
+RESULT_FILE_PREFIX="mdanalysis-distance-spark-"
+HEADER_CSV="Scenario, NumberAtoms, NumberExecutors, Time"
 
 files=["../md_centered.xtc_95Atoms.np_txt", 
        "../md_prod_12x12_everymicroS_pbcmolcenter.xtc_44784Atoms.np_txt", 
        "../vesicle_1_5M_373_stride1000.xtc_145746Atoms.np_txt"]
 
+
+global coord_broadcast
+global cutoff
+
+cutoff = 15.0
 
 ################################################################################
 # Process one point at a time
@@ -28,6 +43,7 @@ def get_edges_point(point_index, adjacency_matrix, cutoff=15.0):
 
 def compute_distance(point_index):
     # 1-D Partitioning
+    global coord_broadcast
     coord_all = coord_broadcast.value
     coord_part = coord_all[point_index-1:point_index]
     #adj = (distance_array(coord_part, coord_all[point_index:], box=None) < cutoff)
@@ -42,6 +58,8 @@ def compute_distance(point_index):
 
 
 def benchmark_spark(coord, NUMBER_EXECUTORS):
+    global coord_broadcast
+    print "******************* RUN WITH %d EXECUTORS ***********"%NUMBER_EXECUTORS
     pilotcompute_description = {
         "service_url": "yarn-client://yarn.radical-cybertools.org",
         "number_of_processes": NUMBER_EXECUTORS,
@@ -60,22 +78,42 @@ def benchmark_spark(coord, NUMBER_EXECUTORS):
     part_rdd.cache()
     start = time.time()
     edges_list=part_rdd.map(compute_distance).flatMap(lambda a: a).collect()
-    print "ComputeDistanceSpark, %d, %d, %.2f"%(len(coord_all), NUMBER_EXECUTORS, (time.time()-start))
+    result="ComputeDistanceSpark, %d, %d, %.2f"%(len(coord_broadcast.value), NUMBER_EXECUTORS, (time.time()-start))
     pilot_spark.cancel()
+    return result
 
-def benchmark_mdanalysis()
+
+def benchmark_mdanalysis():
     start = time.time()
     #distance_array(coord, coord, box=None)
     contact_matrix(coord, returntype="sparse")
     print "ComputeDistanceMDAnalysis, %d, %.2f"%(len(coord), (time.time()-start))
-     
-        
-results=[]
-for f in files:
-    print f
-    coord = np.loadtxt(f, dtype='float32')
-    benchmark_spark(coord, 48)
-    del coord
-    gc.collect()
-    
-    
+       
+
+if __name__ == "__main__":       
+    try:
+        os.mkdir(RESULT_DIR)
+    except:
+        pass        
+
+    results=[]
+    d =datetime.datetime.now()
+    result_filename = RESULT_FILE_PREFIX + d.strftime("%Y%m%d-%H%M%S") + ".csv"
+    f = open(os.path.join(RESULT_DIR, result_filename), "w")
+    f.write(HEADER_CSV+ "\n")
+
+    for i in range(10):
+        for file_name in files:
+            print "Process: " + file_name
+            coord = np.loadtxt(file_name, dtype='float32')
+            for i in [12, 24, 36, 48, 60, 72, 96]:
+                result=benchmark_spark(coord, i)
+                results.append(result)
+                f.write(result + "\n")
+                f.flush()
+            
+            del coord
+            gc.collect()
+
+    f.close()
+    print("Finished run")
